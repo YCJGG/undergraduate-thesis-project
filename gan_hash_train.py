caae922599895e7f3d84981602d62e6f5bca39a6
YCJGG <251623266@qq.com>
2018-03-30 18:07:59 +0800

    networks and data_reader of cgan

--- gan_hash_train.py
+++ gan_hash_train.py
@@ -26,12 +26,29 @@ parser.add_argument('--lrD', type = float, default = 1e-4, help = "learning rate
 parser.add_argument('--lrH', type = float, default = 1e-4, help = "learning rate of Hashnet" )
 parser.add_argument('--beta1', type = float, default = 0.5, help = "beta1 for Adam optimizer" )
 parser.add_argument('--beta2', type = float, default = 0.999, help = "beta2 for Adam optimizer" )
-
+parser.add_argument('--train_epoch', type = int, default = 100, help = "training epochs")
 opt = parser.parse_args()
+
+# load labels
+def LoadLabel(filename):
+    fp = open(filename,'r')
+    labels = [x.strip().split()[2] for x in fp]
+    fp.close()
+    return torch.LongTensor(list(map(int,labels)))
+
+def EncodingOnehot(target, nclasses):
+    target_onehot = torch.FloatTensor(target.size(0), nclasses)
+    target_onehot.zero_()
+    target_onehot.scatter_(1, target.view(-1, 1), 1)
+    return target_onehot
+
 
 #dataloader
 TRAIN_DIR = 'train.list'
 TEST_DIR = 'test.list'
+nclasses = 101
+
+
 train_data = DatasetProcessing(TRAIN_DIR)
 test_data = DatasetProcessing(TEST_DIR)
 
@@ -39,6 +56,11 @@ num_train, num_test = len(train_data) , len(test_data)
 
 train_loader = DataLoader(train_data,batch_size = opt.batch_size, shuffle = True, num_workers = 4)
 test_loader = DataLoader(test_data,batch_size = opt.batch_size, shuffle = False, num_workers = 4)
+
+
+train_labels = LoadLabel(TRAIN_DIR)
+train_labels_onehot = EncodingOnehot(train_labels, nclasses)
+print(train_labels_onehot)
 
 
 G = networks.Generator(opt.g_input_size,opt.g_hidden_size,opt.g_output_size)
@@ -53,12 +75,25 @@ H.cuda()
 
 
 #loss 
-BCE_loss = nn.BCEloss().cuda()
-MSE_loss = nn.MSEloss().cuda()
-L1_loss = nn.L1loss().cuda()
+BCE_loss = nn.BCELoss().cuda()
+MSE_loss = nn.MSELoss().cuda()
+L1_loss = nn.L1Loss().cuda()
+
+
 
 # Adam optimizer
 G_optimizer = optim.Adam(G.parameters(), lr = opt.lrG, betas = (opt.beta1,opt.beta2))
+D_optimizer = optim.Adam(D.parameters(), lr = opt.lrD, betas = (opt.beta1,opt.beta2))
+H_optimizer = optim.Adam(H.parameters(), lr = opt.lrH, betas = (opt.beta1,opt.beta2))
+
+# training
+
+print("###training start~~~~")
+
+
+
+
+#for epoch in range(opt.train_epoch):
 
 
 
