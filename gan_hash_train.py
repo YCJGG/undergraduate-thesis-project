6140ca65d49e7a83b5dce67130e8ec26e6c265d4
YCJGG <251623266@qq.com>
2018-04-02 15:52:59 +0800

    cgan

--- gan_hash_train.py
+++ gan_hash_train.py
@@ -27,6 +27,7 @@ parser.add_argument('--lrH', type = float, default = 1e-4, help = "learning rate
 parser.add_argument('--beta1', type = float, default = 0.5, help = "beta1 for Adam optimizer" )
 parser.add_argument('--beta2', type = float, default = 0.999, help = "beta2 for Adam optimizer" )
 parser.add_argument('--train_epoch', type = int, default = 100, help = "training epochs")
+parser.add_argument('--lamb', type = float, default = 10, help = "lambada")
 opt = parser.parse_args()
 
 # load labels
@@ -60,7 +61,9 @@ test_loader = DataLoader(test_data,batch_size = opt.batch_size, shuffle = False,
 
 train_labels = LoadLabel(TRAIN_DIR)
 train_labels_onehot = EncodingOnehot(train_labels, nclasses)
-print(train_labels_onehot)
+test_labels = LoadLabel(TEST_DIR)
+test_labels_onehot = EncodingOnehot(test_labels, nclasses)
+Y = train_labels_onehot
 
 
 G = networks.Generator(opt.g_input_size,opt.g_hidden_size,opt.g_output_size)
@@ -75,9 +78,10 @@ H.cuda()
 
 
 #loss 
-BCE_loss = nn.BCELoss().cuda()
-MSE_loss = nn.MSELoss().cuda()
-L1_loss = nn.L1Loss().cuda()
+criterionGAN = GANLoss()
+criterionL1 = nn.L1Loss()
+criterionGAN = criterionGAN.cuda()
+criterionL1 = criterionL1.cuda()
 
 
 
@@ -90,10 +94,82 @@ H_optimizer = optim.Adam(H.parameters(), lr = opt.lrH, betas = (opt.beta1,opt.be
 
 print("###training start~~~~")
 
+# initialize the B and H
+B = torch.sign(torch.randn(num_train, opt.bit))
+H_ = torch.zeros(num_train,opt.bit)
 
 
+for epoch in range(opt.train_epoch):
+    # E step
+    temp1 = Y.t().mm(Y) +torch.eye(nclasses)
+    temp1 = temp1.inverse()
+    temp1 = temp1.mm(Y.t())
+    E = temp1.mm(B)
+    #print(D)
+    # B step 
+    B = torch.sign(Y.mm(E) + 1e-5 * H_)
 
-#for epoch in range(opt.train_epoch):
+    G.train()
+    D.train()
+    H.train()
+    #F step
+    for iteration, batch in enumerate(train_loader, 0):
+        ff = batch[0]
+        pf = batch[1]
+        label = batch[2]
+        batch_ind = batch[3]
+
+        ff, pf = Variable(ff.cuda()), Variable(pf.cuda())
+        
+
+        # generate partial feature to full feature
+
+        fakef  = G(pf)
+        ############################
+        # (1) Update D network: maximize log(D(x,y)) + log(1 - D(x,G(x)))
+        ###########################
+        # train generator D
+        D_optimizer.zero_grad()
+        # train with fake
+        fake_ab = torch.cat((pf,fakef),1)
+        # bs * 2 * 4096 
+        pred_fake = D(fake_ab)
+        # bs * 2 * 64
+        loss_d_fake = criterionGAN(pred_fake, False)
+        # train with real
+        real_ab = torch.cat((pf,ff),1)
+        # bs * 2 * 4096
+        pred_real = D(real_ab)
+        # bs * 2 * 64
+        loss_d_real = criterionGAN(pred_real, True)
+        
+        # Combined loss
+        loss_d = (loss_d_fake + loss_d_real) * 0.5
+
+        loss_d.backward()
+
+        D_optimizer.step()
+
+        ############################
+        # (2) Update G network: maximize log(D(x,G(x))) + L1(y,G(x))
+        ##########################
+
+        G_optimizer.zero_grad()
+         # First, G(A) should fake the discriminator
+        fake_ab = torch.cat((pf,fakef),1)
+        
+        pred_fake = D(fake_ab)
+        loss_g_gan = criterionGAN(pred_fake, True)
+         # Second, G(A) = B
+        loss_g_l1 = criterionL1(fakef, ff) * opt.lamb
+        
+        loss_g = loss_g_gan + loss_g_l1
+        
+        loss_g.backward()
+        G_optimizer.step()
+        print("===> Epoch[{}]({}/{}): Loss_D: {:.4f} Loss_G: {:.4f}".format(
+            epoch, iteration, len(train_loader), loss_d.data[0], loss_g.data[0]))
+
 
 
 
